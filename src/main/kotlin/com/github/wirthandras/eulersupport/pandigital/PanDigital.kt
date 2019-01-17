e8d4696be48596eb1e8b683f6b887fe3b4c90ddd
Andras Wirth <agrippa.wia@gmail.com>
2019-01-17 23:22:27 +0100

    Pandigital exceptions added

--- src/main/kotlin/com/github/wirthandras/eulersupport/pandigital/PanDigital.kt
+++ src/main/kotlin/com/github/wirthandras/eulersupport/pandigital/PanDigital.kt
@@ -1,18 +1,32 @@
 package com.github.wirthandras.eulersupport.pandigital
 
-class PanDigital {
+class PanDigital() {
 
-    fun isPandigital(number: Int): Boolean {
+    private var exceptions: Set<Char>? = null
+
+    fun isPandigital(number: Long): Boolean {
         val numberAsString = number.toString()
-        if(numberAsString.length > 10) {
+        if (numberAsString.length > 10) {
             return false
         }
         val set = HashSet<Char>()
         for (c in numberAsString.toCharArray()) {
-            if(!set.add(c)) {
+            if (hitException(c) && !set.add(c)) {
                 return false
             }
         }
         return true
+    }
+
+    private fun hitException(c: Char): Boolean {
+        if (exceptions == null) {
+            return true
+        } else {
+            return exceptions != null && !exceptions!!.contains(c)
+        }
+    }
+
+    fun addExceptions(exceptions: Set<Char>) {
+        this.exceptions = exceptions
     }
 }
--- src/test/kotlin/com/github/wirthandras/eulersupport/pandigital/PanDigitalTest.kt
+++ src/test/kotlin/com/github/wirthandras/eulersupport/pandigital/PanDigitalTest.kt
@@ -2,11 +2,17 @@ package com.github.wirthandras.eulersupport.pandigital
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PanDigitalTest {
 
-    private var test: PanDigital = PanDigital()
+    private lateinit var test: PanDigital
+
+    @BeforeEach
+    fun setUp() {
+        this.test = PanDigital()
+    }
 
     @Test
     fun panDigitalShouldTrue() {
@@ -19,4 +25,17 @@ class PanDigitalTest {
         assertFalse(test.isPandigital(1111))
         assertFalse(test.isPandigital(1010))
     }
+
+    @Test
+    fun panDigitalExceptionShouldFalse() {
+        test.addExceptions(setOf('2'))
+        assertFalse(test.isPandigital(1111))
+        assertFalse(test.isPandigital(1010))
+    }
+
+    @Test
+    fun panDigitalNumberLongerThanTenShouldFalse() {
+        assertFalse(test.isPandigital(12345678901))
+    }
+
 }
